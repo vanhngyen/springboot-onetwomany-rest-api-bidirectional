47912232fd76985a74eb9eb12bda15816de8090c
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2021-05-20 22:29:24 +0700

    a

--- src/main/java/com/example/springbootonetwomanyrestapibidirectional/SpringbootOnetwomanyRestApiBidirectionalApplication.java
+++ src/main/java/com/example/springbootonetwomanyrestapibidirectional/SpringbootOnetwomanyRestApiBidirectionalApplication.java
@@ -2,9 +2,14 @@ package com.example.springbootonetwomanyrestapibidirectional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class SpringbootOnetwomanyRestApiBidirectionalApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootOnetwomanyRestApiBidirectionalApplication.class, args);
